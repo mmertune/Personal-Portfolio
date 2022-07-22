b767686f709593571e6b6868445424210fb9da02
Marvens Mertune <marvensmertune@gmail.com>
2022-07-22 00:49:37 -0400

    add foundation for side menu
    The side menu can be opened up and closed using the burger menu but has bugs that need to be addressed

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const toggle = document.querySelector(".toggle");
+const sideMenu = document.querySelector(".side-menu");
+
 let burgerOpen = false;
 
 burgerMenu.addEventListener("click", () => {
@@ -9,21 +11,37 @@ burgerMenu.addEventListener("click", () => {
     burgerMenu.children[0].style.transform = "rotate(39deg)";
     burgerMenu.children[0].style.transformOrigin = "top left";
     burgerMenu.children[0].style.transitionDuration = "1s";
+    burgerMenu.children[0].style.stroke = "#000000";
 
     burgerMenu.children[1].style.display = "none";
     burgerMenu.children[1].style.transitionDuration = "1s";
+    burgerMenu.children[1].style.stroke = "#000000";
 
     burgerMenu.children[2].style.transform = "rotate(-39deg)";
     burgerMenu.children[2].style.transformOrigin = "bottom left";
     burgerMenu.children[2].style.transitionDuration = "1s";
+    burgerMenu.children[2].style.stroke = "#000000";
+
+    // burgerMenu.children.style.stroke = "#000000"
+    sideMenu.style.display = "flex";
 
     burgerOpen = true;
   } else {
     //close burger menu
     //animate X to burger
     burgerMenu.children[0].style.transform = "rotate(0deg)";
+    burgerMenu.children[0].style.stroke = "#ffffff";
+    burgerMenu.children[0].style.transitionDuration = "1s";
+
     burgerMenu.children[1].style.display = "";
+    burgerMenu.children[1].style.stroke = "#ffffff";
+    burgerMenu.children[1].style.transitionDuration = "1s";
+
     burgerMenu.children[2].style.transform = "rotate(0deg)";
+    burgerMenu.children[2].style.stroke = "#ffffff";
+    burgerMenu.children[2].style.transitionDuration = "1s";
+
+    sideMenu.style.display = "none";
 
     burgerOpen = false;
   }
