46c612cd8f38d56bfd289dfa5943fe6acd5332dc
Marvens Mertune <marvensmertune@gmail.com>
2022-07-25 18:56:34 -0400

    fix burger menu issue
    burger menu would dissapear if user scrolled page while side menu was opened

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
 const burgerMenu = document.querySelector(".burger-menu, .burger-menu2");
+const burgerMenu2 = document.querySelector(".burger-menu2");
+
 const toggle = document.querySelector(".toggle");
 const sideMenu = document.querySelector(".side-menu");
 const sideMenuLinks = document.querySelectorAll(".aside-nav-links a")
@@ -31,35 +33,41 @@ burgerMenu.addEventListener("click", () => {
 
     burgerchild1.transform = "rotate(39deg)";
     burgerchild1.transformOrigin = "top left";
-    burgerchild1.transitionDuration = "1s";
+    burgerchild1.transitionDuration = "0.5s";
     burgerchild1.stroke = "#000000";
 
     burgerchild2.display = "none";
-    burgerchild2.transitionDuration = "1s";
+    burgerchild2.transitionDuration = "0.5s";
     burgerchild2.stroke = "#000000";
 
     burgerchild3.transform = "rotate(-39deg)";
     burgerchild3.transformOrigin = "bottom left";
-    burgerchild3.transitionDuration = "1s";
+    burgerchild3.transitionDuration = "0.5s";
     burgerchild3.stroke = "#000000";
 
     sideMenu.style.display = "flex";
+    if(burgerMenu2){
+      burgerMenu2.style.position = "fixed"
+    }
     burgerOpen = true;
   } else {
     //close burger menu & animate X to burger
     burgerchild1.transform = "rotate(0deg)";
     burgerchild1.stroke = "#ffffff";
-    burgerchild1.transitionDuration = "1s";
+    burgerchild1.transitionDuration = "0.5s";
 
     burgerchild2.display = "";
     burgerchild2.stroke = "#ffffff";
-    burgerchild2.transitionDuration = "1s";
+    burgerchild2.transitionDuration = "0.5s";
 
     burgerchild3.transform = "rotate(0deg)";
     burgerchild3.stroke = "#ffffff";
-    burgerchild3.transitionDuration = "1s";
+    burgerchild3.transitionDuration = "0.5s";
 
     sideMenu.style.display = "none";
+    if(burgerMenu2){
+      burgerMenu2.style.position = "absolute"
+    }
     burgerOpen = false;
   }
 });
